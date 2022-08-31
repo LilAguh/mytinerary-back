8facdf4beb2b14a7be871f0c073c461a421d5e95
LilAguh <aguezeochoa@gmail.com>
2022-08-31 03:22:13 -0300

    se agrega rutas de ciudades

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,13 @@
 var express = require('express');
-const { request } = require('../app');
 var router = express.Router();
-
+var cityRouter = require('./cities')
 /* GET home page. */
 //req = request, peticion del cliente, solo lectura
 //res= response, mensaje que envia el servidor
 router.get('/', function (req, res, next) {
-  res.json([])
+  res.render('index', { title: 'MyTinerary' })
 });
 
-//http://localhost:4000/:id
-//parametro de la url
-router.get('/:id', function (req, res, next) {
-  if (req.params.id == 123) {
-    return res.status(404).json()
-  }
-
-
-  res.json({
-    id: req.params.id
-  })
-});
+router.use('/cities', cityRouter)
 
 module.exports = router;
