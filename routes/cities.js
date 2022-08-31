6888180764a6155599fe4059c17312e2e4fec2a9
LilAguh <aguezeochoa@gmail.com>
2022-08-31 06:01:22 -0300

    se agrega ruta de busqueda por id

--- routes/cities.js
+++ routes/cities.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const { create } = require('../controllers/cityController')
+const { create, read } = require('../controllers/cityController')
 
 /* GET users listing. */
 // router.get('/:id',read)
 router.post('/', create);
+router.get('/:id', read)
 
 module.exports = router;
