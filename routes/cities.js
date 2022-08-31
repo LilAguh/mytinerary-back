982fa019d11b75525c8b29fe36c0ded85b8c47a4
LilAguh <aguezeochoa@gmail.com>
2022-08-31 08:37:04 -0300

    se agregan las rutas para los controladores de los metodos delete, update y allread

--- routes/cities.js
+++ routes/cities.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
-const { create, read } = require('../controllers/cityController')
+const { create, allRead, read, remove, update } = require('../controllers/cityController')
 
 /* GET users listing. */
 // router.get('/:id',read)
-router.post('/', create);
+router.post('/', create)
+router.get('/', allRead)
 router.get('/:id', read)
+router.delete('/:id', remove)
+router.put('/:id', update)
 
 module.exports = router;
